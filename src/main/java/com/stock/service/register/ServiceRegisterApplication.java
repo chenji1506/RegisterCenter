8d85c457fe17aec033c6a037f5d04ef9c16fba9c
zhaoyang <yang.zhao@valueonline.cn>
2018-04-19 11:38:00 +0800

    配置修改

--- src/main/java/com/stock/service/register/ServiceRegisterApplication.java
+++ src/main/java/com/stock/service/register/ServiceRegisterApplication.java
@@ -2,6 +2,7 @@ package com.stock.service.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -9,6 +10,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ServiceRegisterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegisterApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(ServiceRegisterApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 }
